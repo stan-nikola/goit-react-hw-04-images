47999d1039ce1e8e6cf3a73dd617106baeef3d84
Stanislav Karmanov <stanislav.karmanov1@gmail.com>
2022-12-15 20:00:23 +0200

    fix bugs 100500 6

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { Header, SearchButton, SearchInput, Form } from './Searchbar.styled';
+import { Header, SearchButton, SearchInput, Form } from './SearchBar.styled';
 import { BiSearchAlt2 } from 'react-icons/bi';
 
 export function Searchbar({ onSubmit }) {
